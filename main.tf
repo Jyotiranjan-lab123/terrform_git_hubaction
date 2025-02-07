d98d0b7c8c0be0088e812faf4b1e2253ab4b4ae4
Jyoti Ranjan Samal <samaljyotiranjan24@gmail.com>
2025-02-08 02:58:27 +0530

    newvhh

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
-    subscription_id = "dc79e7da-2e89-4a7d-b82e-65398253b785"
-    features {}
+  subscription_id = "dc79e7da-2e89-4a7d-b82e-65398253b785"
+  features {}
 }
 
 resource "azurerm_resource_group" "example" {
-    name     = "example-resources"
-    location = "West Europe"
+  name     = "example-resources"
+  location = "West Europe"
 }
